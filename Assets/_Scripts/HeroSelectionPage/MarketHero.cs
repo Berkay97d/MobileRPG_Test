84281598292f5f7e12c920ade3e747d179b9d786
Berkay Dindar <berkay97d@gmail.com>
2024-09-22 00:19:41 +0300

    Added: Market Hero Click Handler & market hero events

--- Assets/_Scripts/HeroSelectionPage/MarketHero.cs
+++ Assets/_Scripts/HeroSelectionPage/MarketHero.cs
@@ -1,15 +1,51 @@
 using System;
+using _Scripts.HeroSelectionPage;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MarketHero : MonoBehaviour
 {
     [SerializeField] private Transform _mainTransform;
-
+    [SerializeField] private MarketHeroClickHandler _marketHeroClickHandler;
+    
     public event Action<HeroData> OnHeroDataSetted;
     
     private HeroData m_heroData;
-    
-    
+
+    private void Start()
+    {
+        _marketHeroClickHandler.OnLongPressStart += OnLongPressStart;
+        _marketHeroClickHandler.OnLongPressEnd += OnLongPressEnd;
+        _marketHeroClickHandler.OnShortPress += OnShortPress;
+    }
+
+    private void OnDestroy()
+    {
+        _marketHeroClickHandler.OnLongPressStart -= OnLongPressStart;
+        _marketHeroClickHandler.OnLongPressEnd -= OnLongPressEnd;
+        _marketHeroClickHandler.OnShortPress -= OnShortPress;
+    }
+
+    private void OnShortPress()
+    {
+        if (!_mainTransform.gameObject.activeSelf) return;
+        
+    }
+
+    private void OnLongPressEnd()
+    {
+        if (!_mainTransform.gameObject.activeSelf) return;
+        
+    }
+
+    private void OnLongPressStart()
+    {
+        if (!_mainTransform.gameObject.activeSelf) return;
+        
+    }
+
+
     public void SetActiveMarketHero(bool isActive)
     {
         _mainTransform.gameObject.SetActive(isActive);
@@ -21,6 +57,5 @@ public class MarketHero : MonoBehaviour
         
         OnHeroDataSetted?.Invoke(m_heroData);
     }
-    
     
 }
---
+++ Assets/_Scripts/HeroSelectionPage/MarketHeroClickHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _Scripts.HeroSelectionPage
+{
+    public class MarketHeroClickHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        public event Action OnShortPress;  
+        public event Action OnLongPressStart;  
+        public event Action OnLongPressEnd;  
+
+        private static float MS_TIME_THRESHOLD = 3.0f; 
+        private bool m_isHolding;    
+        private float m_holdTime;        
+        private bool m_longPressTriggered; 
+
+        private void Update()
+        {
+            if (!m_isHolding) return;
+            
+            m_holdTime += Time.deltaTime; 
+            
+            if (m_holdTime >= MS_TIME_THRESHOLD && !m_longPressTriggered)
+            {
+                OnLongPressStart?.Invoke(); 
+                m_longPressTriggered = true; 
+            }
+            
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            m_isHolding = true;      
+            m_holdTime = 0f;        
+            m_longPressTriggered = false; 
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            m_isHolding = false;   
+            
+            if (!m_longPressTriggered)
+            {
+                OnShortPress?.Invoke();
+                return;
+            }
+            
+            OnLongPressEnd?.Invoke();
+        }
+
+        
+    }
+}
