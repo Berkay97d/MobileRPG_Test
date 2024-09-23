68dfd0878e1cdd0c1b8c12f4af8d62b7914b1329
Berkay Dindar <berkay97d@gmail.com>
2024-09-23 14:55:18 +0300

    Added: Start & Quit Button

--- Assets/_Scripts/Army.cs
+++ Assets/_Scripts/Army.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace _Scripts
 {
@@ -14,19 +15,21 @@ namespace _Scripts
         private static int MS_MAX_ARMY_COUNT = 3;
         private static List<HeroData> MS_HEROES = new List<HeroData>();
 
-
+        public static event Action<bool> OnArmyChanged; 
 
         public static ArmyActionResult DoAddRemoveArmyActions(HeroData heroData)
         {
             if (MS_HEROES.Contains(heroData))
             {
                 MS_HEROES.Remove(heroData);
+                OnArmyChanged?.Invoke(false);
                 return ArmyActionResult.REMOVE;
             }
             
             if (MS_HEROES.Count >= MS_MAX_ARMY_COUNT) return ArmyActionResult.ARMY_FULL;
             
             MS_HEROES.Add(heroData);
+            OnArmyChanged?.Invoke(MS_HEROES.Count == MS_MAX_ARMY_COUNT);
             return ArmyActionResult.ADDED;
         }
 
---
+++ Assets/_Scripts/Buttons/MenuButtonManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace _Scripts.Buttons
+{
+    public class MenuButtonManager : MonoBehaviour
+    {
+        [SerializeField] private Button _startButton;
+        [SerializeField] private Button _quitButton;
+
+
+        private void Start()
+        {
+            Army.OnArmyChanged += OnArmyChanged;
+            
+            _startButton.interactable = false;
+            _startButton.onClick.AddListener(LoadBattleScene);
+            
+            _quitButton.onClick.AddListener(() =>
+            {
+                Application.Quit();
+            });
+        }
+
+        private void OnDestroy()
+        {
+            Army.OnArmyChanged -= OnArmyChanged;
+        }
+
+        private void OnArmyChanged(bool isArmyFull)
+        {
+            if (isArmyFull)
+            {
+                _startButton.interactable = true;
+                return;
+            }
+            
+            _startButton.interactable = false;
+        }
+
+        private void LoadBattleScene()
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
