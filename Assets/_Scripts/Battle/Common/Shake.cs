521d4e2c86bebb538df3caa3d43d37b10423dd94
Berkay Dindar <berkay97d@gmail.com>
2024-09-24 03:37:58 +0300

    Added: Shake

---
+++ Assets/_Scripts/Battle/Common/Shake.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace _Scripts.Battle
+{
+    public class Shake : MonoBehaviour
+    {
+         private static float shakeDuration = 0.5f;  
+         private static float shakeStrength = 10f;  
+         private static int vibrato = 10;            
+
+        private Vector3 m_startPos;
+        
+        private void Start()
+        {
+            m_startPos = transform.localPosition;
+            BattleHeroAttacker.OnPlayerHit += OnPlayerHit;
+        }
+        
+        private void OnDestroy()
+        {
+            BattleHeroAttacker.OnPlayerHit -= OnPlayerHit;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                DoShake();
+            }
+        }
+        
+        private void OnPlayerHit(HeroData heroData)
+        {
+            DoShake();
+        }
+
+        private void DoShake()
+        {
+            Debug.Log("AAAAAAAAAAAAAAAAA");
+            transform
+                .DOShakePosition(shakeDuration, shakeStrength, vibrato)
+                .OnComplete(() => transform.localPosition = m_startPos); 
+        }
+    }
+}
