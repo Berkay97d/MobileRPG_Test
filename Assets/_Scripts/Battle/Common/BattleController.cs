369a1cef5dfa26e9ab50bd085b240bb6f0530cc7
Berkay Dindar <berkay97d@gmail.com>
2024-09-25 03:47:38 +0300

    Added: Battle Over & Scene Loop

--- Assets/_Scripts/Battle/Common/BattleController.cs
+++ Assets/_Scripts/Battle/Common/BattleController.cs
@@ -7,17 +7,27 @@ namespace _Scripts.Battle
     {
         [SerializeField] private Enemy _enemy;
 
-
+        public event Action<bool> OnFightOver; 
+        
+        private int _heroKillCount;
+        
+        
         private void Start()
         {
             BattleHeroAttacker.OnPlayerHit += OnPlayerHit;
             EnemyAttacker.OnEnemyHit += OnEnemyHit;
+            
+            BattleHero.OnHeroDead += OnHeroDead;
+            Enemy.OnEnemyDead += OnEnemyDead;
         }
-        
+
         private void OnDestroy()
         {
             BattleHeroAttacker.OnPlayerHit -= OnPlayerHit;
             EnemyAttacker.OnEnemyHit -= OnEnemyHit;
+            
+            BattleHero.OnHeroDead -= OnHeroDead;
+            Enemy.OnEnemyDead -= OnEnemyDead;
         }
         
         private void OnEnemyHit(BattleHero battleHero, float damage)
@@ -28,6 +38,21 @@ namespace _Scripts.Battle
         private void OnPlayerHit(HeroData heroData)
         {
             _enemy.GetHealth().Damage(heroData._attackDamage);
+        }
+        
+        private void OnEnemyDead(Enemy enemy)
+        {
+            OnFightOver?.Invoke(true);
+        }
+
+        private void OnHeroDead(BattleHero battleHero)
+        {
+            _heroKillCount++;
+
+            if (_heroKillCount >= 3)
+            {
+                OnFightOver?.Invoke(false);
+            }
         }
     }
 }
---
+++ Assets/_Scripts/Battle/UI/BattleOverUI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace _Scripts.Battle.UI
+{
+    public class BattleOverUI : MonoBehaviour
+    {
+        [SerializeField] private BattleController _battleController;
+        [SerializeField] private Transform _mainTransform;
+        [SerializeField] private TMP_Text _winLoseText;
+        [SerializeField] private Button _menuButton;
+        
+
+        private void Start()
+        {
+            _menuButton.onClick.AddListener(() => SceneManager.LoadScene(0));
+            
+            _battleController.OnFightOver += OnFightOver;
+        }
+
+        private void OnDestroy()
+        {
+            _battleController.OnFightOver -= OnFightOver;
+        }
+
+        private void OnFightOver(bool isWin)
+        {
+            StartCoroutine(UIRoutine());
+            
+            IEnumerator UIRoutine()
+            {
+                yield return new WaitForSeconds(3f);
+
+                _winLoseText.text = isWin ? "YOU WİN!" : "YOU LOST!";
+
+                _mainTransform.gameObject.SetActive(true);
+
+            }
+        }
+    }
+}
